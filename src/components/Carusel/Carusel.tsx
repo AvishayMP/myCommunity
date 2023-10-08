33853398555806b693f1998bed3ceb8589d45f04
ortal calfon <ortal.calfon@gmail.com>
2023-10-08 10:13:48 +0300

    upgrade carusell

--- src/components/Carusel/Carusel.tsx
+++ src/components/Carusel/Carusel.tsx
@@ -1,43 +1,54 @@
 import { useContext, useEffect, useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import { MyContext } from "../ContextProvider";
-import axios from "axios";
+// import { MyContext } from "../ContextProvider";
+// import axios from "axios";
 
 
 function Carusel({ type }: { type: 'image' | 'video' | 'articals' }) {
-    const BaseUrl = useContext(MyContext);
-    const urlsArr: string[] = [];
+    // const BaseUrl = useContext(MyContext);
+    const urlsArr: string[] = ["https://www.youtube.com/embed/Vc4tb44NAGI",
+        "https://www.youtube.com/embed/Vc4tb44NAGI",
+        "https://www.youtube.com/embed/Vc4tb44NAGI", "https://www.youtube.com/embed/Vc4tb44NAGI",
+        "https://www.youtube.com/embed/Vc4tb44NAGI",
+        "https://www.youtube.com/embed/Vc4tb44NAGI"];
     const [index, setIndex] = useState<number>(0);
     useEffect(() => {
-        const data = axios.get(BaseUrl + '/chosen/' + type);
-        urlsArr.push(...data);
+        // const data = axios.get(BaseUrl + '/chosen/' + type);
+        // urlsArr.push(...data);
     }, [])
 
     const handleClick = ({ dir }: { dir: 1 | -1 }): void => {
+        console.log(index);
         setIndex((index + dir + urlsArr.length) % urlsArr.length);
     }
-    return (<>
+    return (<>{type == 'video' &&
         <Container>
             <Row className="align-items-center">
                 <Col onClick={() => handleClick({ dir: -1 })}>
                     &lt;
                 </Col>
-                {urlsArr.map(() => <Col>
-                    <Card>
-                        <iframe
-                            title="Video 1"
-                            className="embed-responsive-item"
-                            src="https://www.youtube.com/embed/Vc4tb44NAGI"
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                    </Card>
-                </Col>)}
+                {urlsArr.map((url, i) => {
+                    return (
+                        Math.abs(index - i) <= 1 && <Col>
+                            <Card>
+                                <iframe
+                                    title="Video 1"
+                                    className="embed-responsive-item"
+                                    src={url}
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                ></iframe>
+                            </Card>
+                        </Col>
+                    );
+                })}
+
                 <Col onClick={() => handleClick({ dir: 1 })}>
                     &gt;
                 </Col>
             </Row>
-        </Container>
+        </Container>}
+        {type !== 'video' && "Need To Define"}
     </>);
 }
 
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,14 +2,17 @@ import { Container, Row } from "react-bootstrap";
 import ContactForm from "./ContactForm";
 import Hero from "./Hero";
 import PhotoGallery from "./PhotoGallery";
-import VideoCarousel from "./VideoCarusel";
+// import VideoCarousel from "./VideoCarusel";
+import Carusel from "./Carusel/Carusel";
 
 function Content() {
     return (
         <>
             <Hero />
             <h1 className="text-center">שיעורי הרב</h1>
-            <VideoCarousel />
+            {/* <VideoCarousel /> */}
+            <Carusel type="video" />
+            <Carusel type="image" />
             <PhotoGallery />
             <Container>
                 <Row className="">
