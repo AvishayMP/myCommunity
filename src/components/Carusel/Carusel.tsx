e43cadb97df0748aeb14b31d5b15a6ca1b77b03e
אבישי פרץ <126172328+AvishayMP@users.noreply.github.com>
2023-10-09 06:06:09 +0000

    Update 4 files

--- src/components/Carusel/Carusel.tsx
+++ src/components/Carusel/Carusel.tsx
@@ -1,55 +1,81 @@
 import { useContext, useEffect, useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
+import Image from "../../../node_modules/react-bootstrap/esm/Image";
 // import { MyContext } from "../ContextProvider";
-// import axios from "axios";
+// import axios from "axios;
 
-
-function Carusel({ type }: { type: 'image' | 'video' | 'articals' }) {
-    // const BaseUrl = useContext(MyContext);
-    const urlsArr: string[] = ["https://www.youtube.com/embed/Vc4tb44NAGI",
-        "https://www.youtube.com/embed/Vc4tb44NAGI",
-        "https://www.youtube.com/embed/Vc4tb44NAGI", "https://www.youtube.com/embed/Vc4tb44NAGI",
-        "https://www.youtube.com/embed/Vc4tb44NAGI",
-        "https://www.youtube.com/embed/Vc4tb44NAGI"];
-    const [index, setIndex] = useState<number>(0);
-    useEffect(() => {
-        // const data = axios.get(BaseUrl + '/chosen/' + type);
-        // urlsArr.push(...data);
-    }, [])
-
-    const handleClick = ({ dir }: { dir: 1 | -1 }): void => {
-        console.log(index);
-        setIndex((index + dir + urlsArr.length) % urlsArr.length);
+type CaruselProps =
+  | {
+      type: "video";
+      arr: string[];
     }
-    return (<>{type == 'video' &&
-        <Container>
-            <Row className="align-items-center">
-                <Col onClick={() => handleClick({ dir: -1 })}>
-                    &lt;
-                </Col>
-                {urlsArr.map((url, i) => {
-                    return (
-                        Math.abs(index - i) <= 1 && <Col>
-                            <Card>
-                                <iframe
-                                    title="Video 1"
-                                    className="embed-responsive-item"
-                                    src={url}
-                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                    allowFullScreen
-                                ></iframe>
-                            </Card>
-                        </Col>
-                    );
-                })}
+  | {
+      type: "image";
+      arr: {
+        src: string;
+        id: number;
+        alt: string
+      }[];
+    }
+  | {
+      type: "articals";
+      arr: string[];
+    };
 
-                <Col onClick={() => handleClick({ dir: 1 })}>
-                    &gt;
-                </Col>
-            </Row>
-        </Container>}
-        {type !== 'video' && "Need To Define"}
-    </>);
+function Carusel(props: CaruselProps) {
+  // const BaseUrl = useContext(MyContext);
+
+  const [index, setIndex] = useState<number>(0);
+  useEffect(() => {
+    // const data = axios.get(BaseUrl + '/chosen/' + type);
+    // urlsArr.push(...data);
+  }, []);
+
+  const handleClick = (dir: 1 | -1): void => {
+    console.log(index);
+    setIndex((index + dir + props.arr.length) % props.arr.length);
+  };
+  return (
+    <>
+      <Container>
+        <Row className="align-items-center">
+          <Col onClick={() => handleClick(-1)}>&lt;</Col>
+
+          {props.arr.map(
+            (url, i) =>
+              (Math.abs(index - i) <= 1 && (
+                <Col>
+                  <Card>
+                    {props.type == "video" && (
+                      <iframe
+                        title="Video 1"
+                        className="embed-responsive-item"
+                        src={url}
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      ></iframe>
+                    )}
+                    {/* {props.type === "image" && (
+                      <Col key={url.id} xs={12} md={6} lg={4}>
+                        <Image
+                          src={url.src}
+                          alt={url.alt}
+                          className="img-fluid"
+                        />
+                        {index < props.arr.length - 1 && <hr className="my-3" />}
+                      </Col>
+                    )} */}
+                    {props.type ==='articals' && <span>Not setted!!! </span>}
+                  </Card>
+                </Col>)
+              ),
+          )}
+
+          <Col onClick={() => handleClick(1)}>&gt;</Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Carusel;
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,25 +1,76 @@
 import { Container, Row } from "react-bootstrap";
 import ContactForm from "./ContactForm";
 import Hero from "./Hero";
-import PhotoGallery from "./PhotoGallery";
+// import PhotoGallery from "./PhotoGallery";
 // import VideoCarousel from "./VideoCarusel";
 import Carusel from "./Carusel/Carusel";
 
 function Content() {
-    return (
-        <>
-            <Hero />
-            <h1 className="text-center">שיעורי הרב</h1>
-            {/* <VideoCarousel /> */}
-            <Carusel type="video" />
-            <Carusel type="image" />
-            <PhotoGallery />
-            <Container>
-                <Row className="">
-                    <ContactForm />
-                </Row>
-            </Container>
-        </>);
+  return (
+    <>
+      <Hero />
+      {/* <h1 className="text-center">שיעורי הרב</h1> */}
+      {/* <VideoCarousel /> */}
+      <Carusel
+        type="video"
+        arr={[
+          "https://www.youtube.com/embed/Vc4tb44NAGI",
+          "https://www.youtube.com/embed/Vc4tb44NAGI",
+          "https://www.youtube.com/embed/Vc4tb44NAGI",
+          "https://www.youtube.com/embed/Vc4tb44NAGI",
+          "https://www.youtube.com/embed/Vc4tb44NAGI",
+          "https://www.youtube.com/embed/Vc4tb44NAGI",
+        ]}
+      />
+      <Carusel
+        type="image"
+        arr={[
+          {
+            id: 1,
+            src: "https://picsum.photos/400",
+            alt: "Image 1",
+            size: "small",
+          },
+          {
+            id: 2,
+            src: "https://picsum.photos/400",
+            alt: "Image 2",
+            size: "large",
+          },
+          {
+            id: 3,
+            src: "https://picsum.photos/400",
+            alt: "Image 3",
+            size: "medium",
+          },
+          {
+            id: 4,
+            src: "https://picsum.photos/400",
+            alt: "Image 1",
+            size: "small",
+          },
+          {
+            id: 5,
+            src: "https://picsum.photos/400",
+            alt: "Image 2",
+            size: "medium",
+          },
+          {
+            id: 6,
+            src: "https://picsum.photos/400",
+            alt: "Image 3",
+            size: "large",
+          },
+        ]}
+      />
+      {/* <PhotoGallery /> */}
+      <Container>
+        <Row>
+          <ContactForm />
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Content;
